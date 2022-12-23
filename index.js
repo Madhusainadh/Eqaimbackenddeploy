c753ac3ae99c0a1506e7e43b19786466ee4b425b
madhusainadh <lalammadhusainadh150@gmail.com>
2022-12-23 14:55:19 +0530

    delpoy backeng for Equim

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const postRouter = require("./Posts/Post.routes")
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(cors());
+mongoose.set("strictQuery", true);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("hello World"));
+app.use("/post", postRouter);
+app.listen(PORT, async () => {
+  await mongoose.connect(
+      process.env.MONGO
+    )
+    .then(() => console.log("User heat the server!!"));
+});
